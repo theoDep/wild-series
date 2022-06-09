142ae6a1c374c7082ae09e4a81d7bfb799eb04b1
THEO DEPELSEMACKER <depelsemackertheo@yahoo.fr>
2022-06-09 11:59:50 +0200

    Finish quest-05

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,10 +9,18 @@ use Symfony\Component\HttpFoundation\Response;
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/program/", name="program_index", methods={"GET"})
      */
   public function index(): Response
   {
     return $this->render('program/index.html.twig', ['website' => 'Wild Series']);
+  }
+
+      /**
+     * @Route("/program/{id}", name="program_show", methods={"GET"}, requirements={"id"="\d+"})
+     */
+  public function show(int $id): Response
+  {
+    return $this->render('program/show.html.twig', ['id' => $id]);
   }
 }
